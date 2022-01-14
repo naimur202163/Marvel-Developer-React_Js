eef4affccae8c4497e4efee5011e44da322e47e4
unknown <you@example.com>
2022-01-14 18:38:28 +0600

    footer is alomost done

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,12 +1,79 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import logo from '../../assets/logo-white.svg';
+import './Footer.css';
 const Footer = () => {
   return (
     <div>
       <Container>
         <Row>
-          <Col></Col>
+          <Col xs={6} lg={2}>
+            <div>
+              <img src={logo} alt="" />
+            </div>
+          </Col>
+          <Col xs={6} lg={2}>
+            <div>
+              <h4 className="overView">OverView</h4>
+              <ul className="ul">
+                <li>Why Marvel</li>
+                <li>Enterprise</li>
+                <li>Priceing</li>
+                <li>requested a damo</li>
+                <li>Explore</li>
+                <li>Apps</li>
+                <li>Developer APi</li>
+              </ul>
+            </div>
+          </Col>
+          <Col xs={6} lg={2}>
+            <div>
+              <h4 className="overView">Features</h4>
+              <ul className="ul">
+                <li>WireFraming</li>
+                <li>Desgin</li>
+                <li>Prototpes</li>
+                <li>requested a damo</li>
+                <li>Explore</li>
+                <li>Apps</li>
+                <li>Developer APi</li>
+              </ul>
+            </div>
+          </Col>
+          <Col xs={6} lg={2}>
+            <div>
+              <h4 className="overView">Support</h4>
+              <ul className="ul">
+                <li>Call center</li>
+                <li>Tearms Of Services</li>
+                <li>Privacy</li>
+              </ul>
+            </div>
+          </Col>
+          <Col xs={6} lg={2}>
+            <div>
+              <h4 className="overView">Marvel</h4>
+              <ul className="ul">
+                <li>Blog </li>
+                <li>Our Team</li>
+                <li>Contact Us</li>
+                <li>success story</li>
+                <li>Brand Guild lines</li>
+              </ul>
+            </div>
+          </Col>
+          <Col xs={6} lg={2}>
+            <div>
+              <h4 className="overView">Follow</h4>
+              <ul className="ul">
+                <li>Twiter </li>
+                <li>Dribble</li>
+                <li>FaceBook</li>
+                <li> github</li>
+                <li>workable</li>
+              </ul>
+            </div>
+          </Col>
         </Row>
       </Container>
     </div>
